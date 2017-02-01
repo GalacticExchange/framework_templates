cd1376c2a3cf4bf1bc53536b2c841f17f6268cd6
Stan Kladko <stan.kladko@galacticexchange.io>
2017-02-01 19:47:28 +0200

    Done

--- hadoop/master/templates/configure.rb
+++ hadoop/master/templates/configure.rb
@@ -1,1 +1,6 @@
-editor = get_editor '/etc/init.sh'
+editor = get_editor '/etc/init.d/cassandra'
+
+editor.file.search_file_replace_line(/^.*ulimit -l unlimited.*$/, '# ulimit -l unlimited')
+editor.search_file_replace_line(/^.*ulimit -l unlimited.*$/, '# ulimit -n "$FD_LIMIT')
+
+
