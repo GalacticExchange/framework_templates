d7482b6af2cf8981f58bfdfe852adc26dac7d89c
Stan Kladko <stan.kladko@galacticexchange.io>
2017-02-02 17:09:48 +0200

    Done

--- hadoop/master/templates/first_run.rb
+++ hadoop/master/templates/first_run.rb
@@ -39,5 +39,7 @@ dexec "sudo usermod -G hadoop hive"
 dexec "rm /etc/cassandra/cassandra-topology.properties"
 
 dexec "cp /etc/hadoop/conf/core-site.xml /etc/impala/conf/core-site.xml"
-dexec "cp /etc/hadoop/conf/hive-site.xml /etc/impala/conf/hive-site.xml"
+
 dexec "cp etc/hadoop/conf/hdfs-site.xml", "/etc/impala/conf/hdfs-site.xml"
+
+dexec "cp /etc/hive/conf/hive-site.xml /etc/impala/conf/hive-site.xml"
