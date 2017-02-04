0f6cd9c155187a188cbe1d36d6ea12d2c2f3d5bd
Stan Kladko <stan.kladko@galacticexchange.io>
2017-02-04 14:47:15 +0200

    Done

--- hadoop/master/templates/first_run.rb
+++ hadoop/master/templates/first_run.rb
@@ -1,57 +1,56 @@
-dexec "service zookeeper-server init"
+dexecs(
+    "service zookeeper-server init",
+    "service hadoop-hdfs-namenode start",
 
+    "hdfs dfsadmin -safemode leave",
 
+    "hadoop fs -mkdir -p /user/hdfs",
+    "sudo -u hdfs hadoop fs -mkdir -p /user/hdfs",
+    "sudo -u hdfs hadoop fs -chown hdfs /user/hdfs",
+    "sudo -u hdfs hadoop fs -mkdir -p /tmp/hadoop-yarn/staging/history/done_intermediate",
+    "sudo -u hdfs hadoop fs -chown -R mapred:mapred /tmp/hadoop-yarn/staging",
+    "sudo -u hdfs hadoop fs -chmod -R 1777 /tmp",
+    "sudo -u hdfs hadoop fs -mkdir -p /var/log/hadoop-yarn",
+    "sudo -u hdfs hadoop fs -chown yarn:mapred /var/log/hadoop-yarn",
+    "sudo -u hdfs hadoop fs -mkdir -p /user/root",
 
-dexec "service hadoop-hdfs-namenode start"
+    # init spark history server
 
-dexec "hdfs dfsadmin -safemode leave"
+    "sudo -u hdfs hadoop fs -mkdir /user/spark",
+    "sudo -u hdfs hadoop fs -mkdir /user/spark/applicationHistory",
+    "sudo -u hdfs hadoop fs -chown -R spark:spark /user/spark",
+    "sudo -u hdfs hadoop fs -chmod 1777 /user/spark/applicationHistory",
+    "sudo -u spark hadoop fs -mkdir -p /user/spark/share/lib",
 
-dexec "hadoop fs -mkdir -p /user/hdfs"
-dexec "sudo -u hdfs hadoop fs -mkdir -p /user/hdfs"
-dexec "sudo -u hdfs hadoop fs -chown hdfs /user/hdfs"
-dexec "sudo -u hdfs hadoop fs -mkdir -p /tmp/hadoop-yarn/staging/history/done_intermediate"
-dexec "sudo -u hdfs hadoop fs -chown -R mapred:mapred /tmp/hadoop-yarn/staging"
-dexec "sudo -u hdfs hadoop fs -chmod -R 1777 /tmp"
-dexec "sudo -u hdfs hadoop fs -mkdir -p /var/log/hadoop-yarn"
-dexec "sudo -u hdfs hadoop fs -chown yarn:mapred /var/log/hadoop-yarn"
-dexec "sudo -u hdfs hadoop fs -mkdir -p /user/root"
+    "sudo -u hdfs hadoop fs -mkdir -p /user/hive/warehouse",
+    "sudo -u hdfs hadoop fs -chmod 1777 /user/hive/warehouse",
+    "sudo -u hdfs hadoop fs -chown hive:hadoop /user/hive/warehouse",
 
-# init spark history server
-dexec "sudo -u hdfs hadoop fs -mkdir /user/spark"
-dexec "sudo -u hdfs hadoop fs -mkdir /user/spark/applicationHistory"
-dexec "sudo -u hdfs hadoop fs -chown -R spark:spark /user/spark"
-dexec "sudo -u hdfs hadoop fs -chmod 1777 /user/spark/applicationHistory"
-dexec "sudo -u spark hadoop fs -mkdir -p /user/spark/share/lib"
+    # ENTERPRISE sudo -u hdfs hadoop fs -mkdir /user/oozie""
+    # ENTERPRISE sudo -u hdfs hadoop fs -chown oozie:oozie /user/oozie""
 
-dexec "sudo -u hdfs hadoop fs -mkdir -p /user/hive/warehouse"
-dexec "sudo -u hdfs hadoop fs -chmod 1777 /user/hive/warehouse"
-dexec "sudo -u hdfs hadoop fs -chown hive:hadoop /user/hive/warehouse"
+    # ENTERPRISE  sudo -u hdfs hadoop fs -mkdir /user/oozie && \
+    # ENTERPRISE  sudo -u hdfs hadoop fs -chown oozie:oozie /user/oozie
 
-# ENTERPRISE sudo -u hdfs hadoop fs -mkdir /user/oozie""
-# ENTERPRISE sudo -u hdfs hadoop fs -chown oozie:oozie /user/oozie""
+    "sudo -u hdfs hadoop fs -mkdir /solr",
+    "sudo -u hdfs hadoop fs -chown solr /solr",
 
-# ENTERPRISE  sudo -u hdfs hadoop fs -mkdir /user/oozie && \
-# ENTERPRISE  sudo -u hdfs hadoop fs -chown oozie:oozie /user/oozie
+    "sudo usermod -G hadoop hive",
 
-dexec "sudo -u hdfs hadoop fs -mkdir /solr"
-dexec "sudo -u hdfs hadoop fs -chown solr /solr"
+    "rm /etc/cassandra/cassandra-topology.properties",
 
-dexec "sudo usermod -G hadoop hive"
+    "cp /etc/hadoop/conf/core-site.xml /etc/impala/conf/core-site.xml",
 
-dexec "rm /etc/cassandra/cassandra-topology.properties"
+    "cp etc/hadoop/conf/hdfs-site.xml /etc/impala/conf/hdfs-site.xml",
 
-dexec "cp /etc/hadoop/conf/core-site.xml /etc/impala/conf/core-site.xml"
+    "cp /etc/hive/conf/hive-site.xml /etc/impala/conf/hive-site.xml",
 
-dexec "cp etc/hadoop/conf/hdfs-site.xml /etc/impala/conf/hdfs-site.xml"
+    "update-alternatives --set sqoop2-tomcat-conf /etc/sqoop2/tomcat-conf.dist",
 
-dexec "cp /etc/hive/conf/hive-site.xml /etc/impala/conf/hive-site.xml"
+    "ln -s /usr/share/java/mysql-connector-java.jar /usr/lib/hive/lib/libmysql-java.jar",
 
-dexec "update-alternatives --set sqoop2-tomcat-conf /etc/sqoop2/tomcat-conf.dist"
-
-dexec "ln -s /usr/share/java/mysql-connector-java.jar /usr/lib/hive/lib/libmysql-java.jar"
-
-dexec "cp /usr/lib/hadoop/etc/hadoop/*.xml /usr/lib/hadoopinput"
+    "cp /usr/lib/hadoop/etc/hadoop/*.xml /usr/lib/hadoopinput"
 
 #COPY download/log4j.properties /etc/hadoop/conf/log4j.properties
-
+)
 
