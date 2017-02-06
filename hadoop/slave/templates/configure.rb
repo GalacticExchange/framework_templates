dbe61d2fe3a4c73d780f110967aadde9043d0889
Stan Kladko <stan.kladko@galacticexchange.io>
2017-02-06 14:22:41 +0200

    Done

--- hadoop/slave/templates/configure.rb
+++ hadoop/slave/templates/configure.rb
@@ -4,16 +4,11 @@ editor = get_editor '/etc/init.d/cassandra'
 editor.search_file_replace_line(/^.*ulimit -l unlimited.*$/, '# ulimit -l unlimited')
 editor.search_file_replace_line(/^.*ulimit -n.*$/, '# ulimit -n "$FD_LIMIT')
 
-editor = get_editor '/etc/default/hive-webhcat-server'
-# noinspection RubyArgumentParentheses
-editor.insert_line_after_match(/^#.*$/, 'export PYTHON_CMD=/usr/bin/python')
-
 editor = get_editor '/etc/security/limits.conf'
 # noinspection RubyArgumentParentheses
 editor.insert_line_after_match(/^#.*$/, 'hdfs  -       nofile  32768')
 # noinspection RubyArgumentParentheses
 editor.insert_line_after_match(/^#.*$/, 'hdfs  -       nproc   2048')
-
 
 editor = get_editor '/etc/pam.d/common-session'
 # noinspection RubyArgumentParentheses
