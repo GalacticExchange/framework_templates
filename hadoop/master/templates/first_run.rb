3f4d53fb4346b7eacceed65b1a6be228c1c0d292
Stan Kladko <stan.kladko@galacticexchange.io>
2017-02-12 19:24:06 +0200

    Done

--- hadoop/master/templates/first_run.rb
+++ hadoop/master/templates/first_run.rb
@@ -9,7 +9,6 @@ dexecs(
     "service hadoop-hdfs-namenode start",
     "service zookeeper-server init --force",
     "sudo usermod -G hadoop hive",
-    "rm /etc/cassandra/cassandra-topology.properties",
     "cp /etc/hadoop/conf/core-site.xml /etc/impala/conf/core-site.xml",
     "cp etc/hadoop/conf/hdfs-site.xml /etc/impala/conf/hdfs-site.xml",
     "cp /etc/hive/conf/hive-site.xml /etc/impala/conf/hive-site.xml",
