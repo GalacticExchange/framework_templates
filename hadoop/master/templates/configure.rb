e9e0e15cf25f80b18102b15e870b203451b1750b
Stan Kladko <stan.kladko@galacticexchange.io>
2017-02-16 18:12:29 +0200

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	create_cluster.yml

--- hadoop/master/templates/configure.rb
+++ hadoop/master/templates/configure.rb
@@ -1,5 +1,5 @@
 
-editor = Common.get_editor '/etc/init.d/cassandra'
+editor = get_editor '/etc/init.d/cassandra'
 
 editor.search_file_replace_line(/^.*ulimit -l unlimited.*$/, '# ulimit -l unlimited')
 editor.search_file_replace_line(/^.*ulimit -n.*$/, '# ulimit -n "$FD_LIMIT')
