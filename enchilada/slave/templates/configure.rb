1f773634de6a08e7c770456316999bd459f6bf83
Stan Kladko <stan.kladko@galacticexchange.io>
2017-02-28 11:45:59 +0200

    Done

--- enchilada/slave/templates/configure.rb
+++ enchilada/slave/templates/configure.rb
@@ -1,20 +1,5 @@
-editor = get_editor '/etc/init.d/cassandra'
-
-editor.search_file_replace_line(/^.*ulimit -l unlimited.*$/, '# ulimit -l unlimited')
-editor.search_file_replace_line(/^.*ulimit -n.*$/, '# ulimit -n "$FD_LIMIT')
-
-editor = get_editor '/etc/security/limits.conf'
-# noinspection RubyArgumentParentheses
-editor.insert_line_after_match(/^#.*$/, 'hdfs  -       nofile  32768')
-# noinspection RubyArgumentParentheses
-editor.insert_line_after_match(/^#.*$/, 'hdfs  -       nproc   2048')
-
-editor = get_editor '/etc/pam.d/common-session'
-# noinspection RubyArgumentParentheses
-editor.insert_line_after_match(/^# end of pam-auth-update config.*$/, 'session required  pam_limits.so')
-
-
 # ENTERPRISSE RUN \
 #        echo "hbase -       nofile  32768" >> /etc/security/limits.conf && \
 #        echo "hbase -       nproc   2048" >> /etc/security/limits.conf && \
 #        echo "session required  p
+
