e85999a3c1f44c440bbaf3c87c1bf50d0fba67b1
Kateryna Khotkevych <kateryna.khotkevych@galacticexchange.io>
2017-02-28 14:00:52 +0200

    enchilada init commit

--- enchilada/slave/templates/configure.rb
+++ enchilada/slave/templates/configure.rb
@@ -1,5 +1,1 @@
-# ENTERPRISSE RUN \
-#        echo "hbase -       nofile  32768" >> /etc/security/limits.conf && \
-#        echo "hbase -       nproc   2048" >> /etc/security/limits.conf && \
-#        echo "session required  p
 
