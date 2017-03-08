2651d02562de8bdbc373be0a5fa907699f808f6a
Stan Kladko <stan.kladko@galacticexchange.io>
2017-03-08 21:37:32 +0200

    Done

--- hadoop/master/templates/first_run.rb
+++ hadoop/master/templates/first_run.rb
@@ -5,11 +5,8 @@ dexecs_parallel(
     "cp -f /etc/hadoop/conf/core-site.xml /etc/impala/conf/core-site.xml",
     "cp -f /etc/hadoop/conf/hdfs-site.xml /etc/impala/conf/hdfs-site.xml",
     "cp -f /etc/hive/conf/hive-site.xml /etc/impala/conf/hive-site.xml",
-    "update-alternatives --set sqoop2-tomcat-conf /etc/sqoop2/tomcat-conf.dist",
+    "update-alternatives --set sqoop2-tomcat-conf /etc/sqoop2/tomcat-conf.dist"
 )
-
-dexec "service hadoop-hdfs-namenode start"
-
 
 #dexec "hdfs dfsadmin -safemode leave | true"
 
