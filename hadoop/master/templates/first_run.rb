d7a0523ded359f364907314c3a6e2798d35433ae
Stan Kladko <stan.kladko@galacticexchange.io>
2017-03-08 20:51:12 +0200

    Done

--- hadoop/master/templates/first_run.rb
+++ hadoop/master/templates/first_run.rb
@@ -1,9 +1,7 @@
 dexecs_parallel(
-    "sudo -u hdfs hdfs namenode -format -force",
     "bash -c 'cat /tmp/hadoop_rsa_public >>  /root/.ssh/authorized_keys && chmod 600 /root/.ssh/authorized_keys'",
     "bash -c 'cp -f /tmp/hadoop_rsa /root/.ssh/ && chmod 600 /root/.ssh/hadoop_rsa'",
     "service zookeeper-server init --force",
-    "sudo usermod -G hadoop hive",
     "cp -f /etc/hadoop/conf/core-site.xml /etc/impala/conf/core-site.xml",
     "cp -f /etc/hadoop/conf/hdfs-site.xml /etc/impala/conf/hdfs-site.xml",
     "cp -f /etc/hive/conf/hive-site.xml /etc/impala/conf/hive-site.xml",
@@ -11,37 +9,11 @@ dexecs_parallel(
 )
 dexec "service hadoop-hdfs-namenode start"
 
-is_port_open?("127.0.0.1", 8020, 10000)
 
 #dexec "hdfs dfsadmin -safemode leave | true"
 
-dexecs_parallel(
-    "sudo -u hdfs hadoop fs -mkdir -p /user/hdfs " +
-    "/tmp/hadoop-yarn/staging/history/done_intermediate " +
-    "/var/log/hadoop-yarn  /user/root  /user/spark /solr ",
-    "sudo -u hdfs hadoop fs -mkdir -p /user/spark/applicationHistory " +
-        " /user/spark/share/lib  /user/hive/warehouse"
-)
-
-dexecs_parallel(
-    "sudo -u hdfs hadoop fs -chown hdfs /user/hdfs",
-    "sudo -u hdfs hadoop fs -chown -R mapred:mapred /tmp/hadoop-yarn/staging",
-    "sudo -u hdfs hadoop fs -chmod -R 1777 /tmp",
-    "sudo -u hdfs hadoop fs -chown yarn:mapred /var/log/hadoop-yarn",
-    "sudo -u hdfs hadoop fs -chown -R spark:spark /user/spark",
-    "sudo -u hdfs hadoop fs -chmod 1777 /user/spark/applicationHistory",
-    "sudo -u hdfs hadoop fs -chmod 1777 /user/hive/warehouse",
-    "sudo -u hdfs hadoop fs -chown hive:hadoop /user/hive/warehouse",
-
-    # ENTERPRISE sudo -u hdfs hadoop fs -mkdir /user/oozie""
-    # ENTERPRISE sudo -u hdfs hadoop fs -chown oozie:oozie /user/oozie""
-
-    # ENTERPRISE  sudo -u hdfs hadoop fs -mkdir /user/oozie && \
-    # ENTERPRISE  sudo -u hdfs hadoop fs -chown oozie:oozie /user/oozie
-
-    "sudo -u hdfs hadoop fs -chown solr /solr"
 
 
 #COPY download/log4j.properties /etc/hadoop/conf/log4j.properties
-)
+
 
