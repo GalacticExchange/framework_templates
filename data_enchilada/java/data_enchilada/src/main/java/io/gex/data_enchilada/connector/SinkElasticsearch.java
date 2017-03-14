f50e6adaeb29cb035dbb01b9d68d92dab20eef08
Kateryna Khotkevych <kateryna.khotkevych@galacticexchange.io>
2017-03-14 17:11:58 +0200

    cluster name

--- data_enchilada/java/data_enchilada/src/main/java/io/gex/data_enchilada/connector/SinkElasticsearch.java
+++ data_enchilada/java/data_enchilada/src/main/java/io/gex/data_enchilada/connector/SinkElasticsearch.java
@@ -6,7 +6,6 @@ import org.apache.commons.io.FileUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.elasticsearch.client.Client;
-import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.InetSocketTransportAddress;
 import org.elasticsearch.transport.client.PreBuiltTransportClient;
