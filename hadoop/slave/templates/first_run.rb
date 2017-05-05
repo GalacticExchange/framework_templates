e4816220c4dc53c5a5d693e3d6256dcc3818a0aa
Kateryna Khotkevych <kateryna.khotkevych@galacticexchange.io>
2017-05-05 16:19:09 +0300

    elastic fix

--- hadoop/slave/templates/first_run.rb
+++ hadoop/slave/templates/first_run.rb
@@ -5,5 +5,3 @@ dexec "cp /etc/hadoop/conf/core-site.xml /etc/impala/conf/core-site.xml"
 dexec "cp etc/hadoop/conf/hdfs-site.xml /etc/impala/conf/hdfs-site.xml"
 
 dexec "cp /etc/hive/conf/hive-site.xml /etc/impala/conf/hive-site.xml"
-
-#dexec "chmod 777 /etc/init.d/elasticsearch"
