b5a0ffc900168ba97226a8687f416d266131ce14
sergey <sergey.nazarov@galacticexchange.io>
2017-05-12 18:26:58 +0300

    Added scraper

--- scraper/docker/metadata.rb
+++ scraper/docker/metadata.rb
@@ -1,15 +1,15 @@
 data = {
     "cluster.uid" => { description: "Cluster ID", default_value: "{{cluster.uid}}", mandatory: 1, editable: 0},
-    "cluster.name" => { description: "Cluster name", default_value: "{{cluster.name}}", mandatory: 1, editable: 0}
-    #"master.host" => { description: "Host of hadoop master node", default_value: "{{master.host}}", mandatory: 1, basic: 1, editable: 0,},
-    #"master.ip" => { description: "IP of Hadoop master node", default_value: "{{master.ip}}", mandatory: 1, basic: 1, editable: 0,},
-    #"slave.ip" => { description: "IP of Hadoop slave node", default_value: "{{slave.ip}}", mandatory: 1, basic: 1, editable: 0,},
+    "cluster.name" => { description: "Cluster name", default_value: "{{cluster.name}}", mandatory: 1, editable: 0},
+    "elastic_index_name" => { description: "Elastic Search index name", default_value: "scraper", mandatory: 1, editable: 1},
+    "scrap_urls" => { description: "URLs for scrapping (please separate with ,)", default_value: "", mandatory: 1, editable: 1},
+    "scrap_domains" => { description: "Domains for scrapping urls (Needs for not to scrap external websites)", default_value: "", mandatory: 1, editable: 1},
+    "scrap_num_rounds" => { description: "The number of rounds to scrap", default_value: "2", mandatory: 1, editable: 1},
 }
 
 config_params(data)
 
 ### services
 services({
-             'webui' => {name: 'webui', protocol: 'http', port: 80},
              'ssh' => {title: 'SSH', name: 'ssh', protocol: 'ssh', port: 22},
          })
