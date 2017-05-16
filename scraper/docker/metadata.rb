34bb2c7fb6b8a5318c7a18576d8e187c2044f444
sergey <sergey.nazarov@galacticexchange.io>
2017-05-16 18:34:14 +0300

    Crawl sitemap

--- scraper/docker/metadata.rb
+++ scraper/docker/metadata.rb
@@ -1,11 +1,10 @@
 data = {
     "cluster.uid" => { description: "Cluster ID", default_value: "{{cluster.uid}}", mandatory: 1, editable: 0},
     "cluster.name" => { description: "Cluster name", default_value: "{{cluster.name}}", mandatory: 1, editable: 0},
-    "elastic_index_name" => { description: "Elastic Search index name", default_value: "scraper", mandatory: 1, editable: 1},
-    "scrap_urls" => { description: "URLs for scrapping (please separate with ,)", default_value: "", mandatory: 1, editable: 1},
-    "scrap_domains" => { description: "Domains for scrapping urls (Needs for not to scrap external websites)", default_value: "", mandatory: 1, editable: 1},
-    "scrap_num_rounds" => { description: "The number of rounds to scrap", default_value: "2", mandatory: 1, editable: 1},
-    "scrap_interval" => { description: "Time between scrapping in seconds (1m - minute, 1h - hour)", default_value: "1h", mandatory: 1, editable: 1},
+    "elastic.index_name" => { description: "Elastic Search index name", default_value: "scraper", mandatory: 1, editable: 1},
+    "scrap.urls" => { description: "URLs for scrapping (please separate with ,)", default_value: "https://ru.tsn.ua/ukrayina,http://news.liga.net/all/politics/", mandatory: 1, editable: 1},
+    "scrap.num_rounds" => { description: "The number of rounds to scrap", default_value: "2", mandatory: 1, editable: 1},
+    "scrap.interval" => { description: "Time between scrapping in seconds (1m - minute, 1h - hour)", default_value: "1h", mandatory: 1, editable: 1},
 }
 
 config_params(data)
