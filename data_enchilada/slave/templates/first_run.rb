4244ff3edae69221c92b493ce93fbcaed6ac0944
Kateryna Khotkevych <kateryna.khotkevych@galacticexchange.io>
2017-05-22 12:25:17 +0300

    install mysql to dataenchilada

--- data_enchilada/slave/templates/first_run.rb
+++ data_enchilada/slave/templates/first_run.rb
@@ -1,3 +1,5 @@
-dexec "cd /var/www/apps/data_enchilada/config; rake db:create"
-
-dexec "cd /var/www/apps/data_enchilada/config; rake db:migrate"
+dexec "service mysql start"
+dexec "mysql -e \"CREATE USER 'enchilada'@'localhost' IDENTIFIED BY 'Galactic1';\""
+dexec "mysql -e \"GRANT ALL PRIVILEGES ON *.* TO 'enchilada'@'localhost';\""
+dexec "/bin/bash -c 'source /etc/profile.d/rvm.sh; cd /var/www/apps/data_enchilada/config; rake db:create'"
+dexec "/bin/bash -c 'source /etc/profile.d/rvm.sh; cd /var/www/apps/data_enchilada/config; rake db:migrate'"
