a902e8a8c7a4167c2d5a273f782f4ecff04c3b1c
Kateryna Khotkevych <kateryna.khotkevych@galacticexchange.io>
2017-05-26 14:23:27 +0300

    da default properties

--- data_enchilada/docker/metadata.rb
+++ data_enchilada/docker/metadata.rb
@@ -1,7 +1,7 @@
 data = {
     "elasticsearch.host" => { description: "Elasticsearch host", default_value: "{{elasticsearch.host}}", mandatory: 0, editable: 1},
     "elasticsearch.port" => { description: "Elasticsearch REST port", default_value: "9200", mandatory: 0, editable: 1},
-    "kafka.host" => { description: "Kafka host", default_value: "{{kafka.host}}", mandatory: 0, editable: 1},
+    "kafka.host" => { description: "Kafka host", default_value: "{{kafka.broker}}", mandatory: 0, editable: 1},
     "kafka.port" => { description: "Kafka port", default_value: "9092", mandatory: 0, editable: 1},
     "zookeeper.host" => { description: "Zookeper host", default_value: "{{zookeeper.host}}", mandatory: 0, editable: 1},
     "zookeeper.port" => { description: "Zookeper port", default_value: "2181", mandatory: 0, editable: 1},
@@ -11,6 +11,8 @@ data = {
     "cassandra.password" => { description: "Cassandra password", default_value: "cassandra", mandatory: 0, editable: 1},
     "hdfs.host" => { description: "HDFS host", default_value: "{{hdfs.host}}", mandatory: 0, editable: 1},
     "hdfs.port" => { description: "HDFS port", default_value: "50070", mandatory: 0, editable: 1},
+    "kudu.master_host" => { description: "Kudu master host", default_value: "{{kudu.master_host}}", mandatory: 0, editable: 1},
+    "kudu.master_port" => { description: "Kudu master port", default_value: "7051", mandatory: 0, editable: 1},
 }
 
 #todo use predefine ports
