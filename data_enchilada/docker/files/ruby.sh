4698c3a82413e53cbfda0450f49d9791de5c62cd
Kateryna Khotkevych <kateryna.khotkevych@galacticexchange.io>
2017-05-31 12:22:13 +0300

    cleanong de + nifi election time

--- data_enchilada/docker/files/ruby.sh
+++ data_enchilada/docker/files/ruby.sh
@@ -4,7 +4,7 @@
 
 source /etc/profile.d/rvm.sh
 
-# add to /root/.bash_rc ??
+echo "source /etc/profile.d/rvm.sh" >> /etc/bash.bashrc
 
 rvm install ruby-2.3.3
 
@@ -14,11 +14,5 @@ gem install bundle bundler rake mysql2
 
 # fluent plugin
 rvm wrapper ruby-2.3.3 boot fluentd
-
-# check
-#which fluentd
-#/usr/local/rvm/gems/ruby-2.3.3/bin/fluentd
-#boot_fluentd --version
-
 
 cd /var/www/apps/data_enchilada && bundle install
