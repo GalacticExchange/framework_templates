7e7fda562bd919b116175e242a0a0b3c027005ad
sergey <sergey.nazarov@galacticexchange.io>
2017-06-20 15:44:01 +0300

    New scrappe config

--- scraper/docker/metadata.rb
+++ scraper/docker/metadata.rb
@@ -1,17 +1,13 @@
 data = {
     "elasticsearch.host" => { description: "Elasticsearch host", default_value: "{{elasticsearch.host}}", mandatory: 1, editable: 1},
     "elasticsearch.port" => { description: "Elasticsearch port (NOT! REST port)", default_value: "9300", mandatory: 1, editable: 1},
-    "elasticsearch.cluster_name" => { description: "Elasticsearch cluster name. Default value for embadded Elasticsearch id the cluster name", default_value: "{{cluster.name}}", mandatory: 1, editable: 1},
-    "elasticsearch.index_name" => { description: "Elasticsearch index name", default_value: "scraper", mandatory: 1, editable: 1},
-    "scrap.urls" => { description: "URLs for scrapping (please separate with ,)", default_value: "http://galacticexchange.io/", mandatory: 1, editable: 1},
-    "scrap.depth" => { description: "Depth to scrap", default_value: "2", mandatory: 1, editable: 1},
-    "scrap.interval" => { description: "Time between scrapping in seconds (1m - minute, 1h - hour)", default_value: "1h", mandatory: 1, editable: 1},
-    "scrap.extract_article" => { description: "Extract articles from pages", default_value: "false", mandatory: 1, editable: 1},
+    "elasticsearch.cluster_name" => { description: "Elasticsearch cluster name. Default value for embadded Elasticsearch id the cluster name", default_value: "{{cluster.name}}", mandatory: 1, editable: 1}
 }
 
 config_params(data)
 
 ### services
 services({
+             'webui' => {name: 'Web UI', protocol: 'http', port: 80},
              'ssh' => {title: 'SSH', name: 'ssh', protocol: 'ssh', port: 22},
          })
