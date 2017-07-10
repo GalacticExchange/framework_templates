e9a7fb3670fee7c4f92dd1e29800bda8c122634b
Kateryna Khotkevych <kateryna.khotkevych@galacticexchange.io>
2017-07-10 15:32:34 +0300

    fix

---
+++ hadoop/slave/templates/etc/neo4j/pass.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+/usr/bin/curl -H "Content-Type: application/json" -X POST -d '{"password":"password"}' -u neo4j:neo4j http://localhost:7474/user/neo4j/password >> /tmp/curl.txt
+/usr/bin/curl -H "Content-Type: application/json" -X POST -d '{"password":"neo4j"}' -u neo4j:password http://localhost:7474/user/neo4j/password >> /tmp/curl.txt
