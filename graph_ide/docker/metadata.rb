c9fdb7482ee2efdfec582d28862c58fe57d409b1
sergey <sergey.nazarov@galacticexchange.io>
2017-07-10 17:27:08 +0300

    Update config

--- graph_ide/docker/metadata.rb
+++ graph_ide/docker/metadata.rb
@@ -1,6 +1,6 @@
 data = {
-    "neo4j.host" => {description: "neo4j host", default_value: "{{neo4j.host}}", mandatory: 1, editable: 0},
-    "neo4j.port" => {description: "neo4j port", default_value: "{{neo4j.port}}", mandatory: 1, editable: 0}
+    "neo4j.host" => {description: "neo4j host", default_value: "{{neo4j.host}}", mandatory: 1, editable: 1},
+    "neo4j.port" => {description: "neo4j port", default_value: "{{neo4j.port}}", mandatory: 1, editable: 1}
 }
 
 config_params(data)
@@ -10,3 +10,16 @@ services({
              'webui' => {name: 'Web UI', protocol: 'http', port: 80},
              'ssh' => {title: 'SSH', name: 'ssh', protocol: 'ssh', port: 22},
          })
+
+app_info = {
+    metrics: {
+        graph_ide: {
+            memory: 0.5,
+            hdd: 1.0
+        }
+    },
+    dependencies: {
+        components: [:neo4j],
+        containers: []
+    }
+}
