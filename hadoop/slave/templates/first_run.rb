815a3f23b7d5c18e81522778167e3329ba551ab0
Kateryna Khotkevych <kateryna.khotkevych@galacticexchange.io>
2017-07-10 17:16:16 +0300

    neo4j fix

--- hadoop/slave/templates/first_run.rb
+++ hadoop/slave/templates/first_run.rb
@@ -7,17 +7,9 @@ dexec "cp etc/hadoop/conf/hdfs-site.xml /etc/impala/conf/hdfs-site.xml"
 dexec "cp /etc/hive/conf/hive-site.xml /etc/impala/conf/hive-site.xml"
 
 if get_value('_components').include?('nifi')
-  # nifi
   dexec "/bin/bash -c 'chmod 777 /usr/local/nifi/bin/nifi-env.sh'"
 end
 
 if get_value('_components').include?('superset')
-  # superset
   dexec "/home/superset/.bin/superset-init"
 end
-
-#if get_value('_components').include?('neo4j')
-  # neo4j
-  #dexec "/bin/bash -c 'chmod 777 /etc/neo4j/pass.sh'"
-  #dexec "/etc/neo4j/pass.sh'"
-#end
