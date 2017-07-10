8ff7abe0b9195f3510ae171509a311a233a06ec6
Kateryna Khotkevych <kateryna.khotkevych@galacticexchange.io>
2017-07-10 13:23:20 +0300

    neo4j fix

--- hadoop/slave/templates/first_run.rb
+++ hadoop/slave/templates/first_run.rb
@@ -6,9 +6,12 @@ dexec "cp etc/hadoop/conf/hdfs-site.xml /etc/impala/conf/hdfs-site.xml"
 
 dexec "cp /etc/hive/conf/hive-site.xml /etc/impala/conf/hive-site.xml"
 
+# nifi
+dexec "chmod 777 /usr/local/nifi/bin/nifi-env.sh"
+
 # superset
 dexec "/home/superset/.bin/superset-init"
 
 # neo4j
-dexec "curl -H \"Content-Type: application/json\" -X POST -d '{\"password\":\"password\"}' -u neo4j:neo4j http://10.1.0.82:7474/user/neo4j/password"
-dexec "curl -H \"Content-Type: application/json\" -X POST -d '{\"password\":\"neo4j\"}' -u neo4j:password http://10.1.0.82:7474/user/neo4j/password"
+dexec "curl -H 'Content-Type: application/json' -X POST -d '{\"password\":\"password\"}' -u neo4j:neo4j http://localhost:7474/user/neo4j/password"
+dexec "curl -H 'Content-Type: application/json' -X POST -d '{\"password\":\"neo4j\"}' -u neo4j:password http://localhost:7474/user/neo4j/password"
