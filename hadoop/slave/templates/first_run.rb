4a42663f1f3c2118dda8bce95f2a63b59253f503
Kateryna Khotkevych <kateryna.khotkevych@galacticexchange.io>
2017-08-11 12:12:35 +0300

    GEX-1531 bashrc

--- hadoop/slave/templates/first_run.rb
+++ hadoop/slave/templates/first_run.rb
@@ -1,6 +1,7 @@
 dexec "/bin/bash -c 'echo \"export TERM=xterm\" >> /etc/bash.bashrc'"
 dexec "/bin/bash -c \"echo $'\nexport PYTHONPATH=/home/superset/.superset:$PYTHONPATH' >> /etc/profile\""
 dexec "/bin/bash -c \"echo $'\nexport PYTHONPATH=/home/superset/.superset:$PYTHONPATH' >> /etc/environment\""
+dexec "/bin/bash -c \"echo $'\nexport PYTHONPATH=/home/superset/.superset:$PYTHONPATH' >> /etc/bash.bashrc\""
 # source /etc/profile
 
 dexec "cp /etc/hadoop/conf/core-site.xml /etc/impala/conf/core-site.xml"
